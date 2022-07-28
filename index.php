dbd969da21fe3914d7076c9f3165973b3184c66e
l3aobab <noelonazo@gmail.com>
2022-07-28 16:52:40 +0200

    Bootstrap and css changes

--- index.php
+++ index.php
@@ -5,17 +5,13 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>O parte</title>
 	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="main.css">
 </head>
-<body>
-
+<body class="bg-black">
 	<?php 
-		//$url = 'https://newsapi.org/v2/everything?q=formula=1&apiKey=5b4f8bdf68e64633a1a878e95615e0af';
-		//$response = file_get_contents('https://newsapi.org/v2/everything?q=formula=1&apiKey=5b4f8bdf68e64633a1a878e95615e0af');
-		//$NewsData = json_decode($response,true);
-
 		$ch = curl_init();
 		$agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36';
-		curl_setopt($ch, CURLOPT_URL, "https://newsapi.org/v2/everything?q=formula=1&apiKey=5b4f8bdf68e64633a1a878e95615e0af");
+		curl_setopt($ch, CURLOPT_URL, "https://newsapi.org/v2/everything?q=formula=1&sortBy=publishedAt&apiKey=5b4f8bdf68e64633a1a878e95615e0af");
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_USERAGENT, $agent);
 
@@ -23,32 +19,31 @@
 		$response = json_decode($data)
 		
 	?>
-	<div class="news-grid">
-		<div class="news-header">
-			<h1>Las noticias</h1>
+	<div class="container">
+		<div class="row">
+			<div class="col header">
+				<h1><b>O Parte</b></h1>
+			</div>
 		</div>
 		
-		<?php 
+		<?php
+
 			foreach($response->articles as $news) {
 		?>
-		<div class="news-display">
-			<div class="image">
+		<div class="row new bg-dark">
+			<div class="col content">
 				<img src="<?php echo $news->urlToImage; ?>" alt="news-thumbnail">
 			</div>
-			<div class="article">
-				<h2><?php echo $news->title; ?></h2>
-				<h4><?php echo $news->description; ?></h4>
-				<p><?php echo $news->content; ?></p>
-				<h5><?php echo $news->publishedAt; ?></h5>
-				<a href="<?php echo $news->url; ?>">Seguir leyendo</a>
+			<div class="col-8 content">
+				<h3><?php echo $news->title; ?></h3>
+				<h5><?php echo $news->description; ?></h5>
+				<h7><?php echo $news->publishedAt; ?>&nbsp;<a href="<?php echo $news->url; ?>">Seguir leyendo</a></h7>
+				
 			</div>
 		</div>
 		<?php 
 			}
 		?>
-		
-		<div class="news-footer">
-		</div>
 	</div>
 
 	
