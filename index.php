cc115e81233485e4a004f6df8cdb298f0feeb41e
l3aobab <noelonazo@gmail.com>
2022-08-01 14:53:19 +0200

    Default news

--- index.php
+++ index.php
@@ -9,10 +9,10 @@
 </head>
 <body class="bg-black">
 	<?php 
-		function business() {
+		function topHeadlines() {
 			$ch = curl_init();
 			$agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36';
-			curl_setopt($ch, CURLOPT_URL, "https://newsapi.org/v2/top-headlines?category=business&language=es&sortBy=publishedAt&apiKey=5b4f8bdf68e64633a1a878e95615e0af");
+			curl_setopt($ch, CURLOPT_URL, "https://newsapi.org/v2/top-headlines?language=es&apiKey=5b4f8bdf68e64633a1a878e95615e0af");
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 			curl_setopt($ch, CURLOPT_USERAGENT, $agent);
 
@@ -21,85 +21,6 @@
 
 			return $res;
 		}
-
-		function entertainment() {
-			$ch = curl_init();
-			$agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36';
-			curl_setopt($ch, CURLOPT_URL, "https://newsapi.org/v2/top-headlines?category=entertainment&language=es&sortBy=publishedAt&apiKey=5b4f8bdf68e64633a1a878e95615e0af");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-
-			$data = curl_exec($ch);
-			$res = json_decode($data);
-
-			return $res;
-		}
-		
-		function general() {
-			$ch = curl_init();
-			$agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36';
-			curl_setopt($ch, CURLOPT_URL, "https://newsapi.org/v2/top-headlines?category=general&language=es&sortBy=publishedAt&apiKey=5b4f8bdf68e64633a1a878e95615e0af");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-
-			$data = curl_exec($ch);
-			$res = json_decode($data);
-
-			return $res;
-		}
-
-		function health() {
-			$ch = curl_init();
-			$agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36';
-			curl_setopt($ch, CURLOPT_URL, "https://newsapi.org/v2/top-headlines?category=health&language=es&sortBy=publishedAt&apiKey=5b4f8bdf68e64633a1a878e95615e0af");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-
-			$data = curl_exec($ch);
-			$res = json_decode($data);
-
-			return $res;
-		}
-
-		function science() {
-			$ch = curl_init();
-			$agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36';
-			curl_setopt($ch, CURLOPT_URL, "https://newsapi.org/v2/top-headlines?category=science&language=es&sortBy=publishedAt&apiKey=5b4f8bdf68e64633a1a878e95615e0af");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-
-			$data = curl_exec($ch);
-			$res = json_decode($data);
-
-			return $res;
-		}
-
-		function sports() {
-			$ch = curl_init();
-			$agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36';
-			curl_setopt($ch, CURLOPT_URL, "https://newsapi.org/v2/top-headlines?category=sports&language=es&sortBy=publishedAt&apiKey=5b4f8bdf68e64633a1a878e95615e0af");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-
-			$data = curl_exec($ch);
-			$res = json_decode($data);
-
-			return $res;
-		}
-
-		function technology() {
-			$ch = curl_init();
-			$agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36';
-			curl_setopt($ch, CURLOPT_URL, "https://newsapi.org/v2/top-headlines?category=technology&language=es&sortBy=publishedAt&apiKey=5b4f8bdf68e64633a1a878e95615e0af");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_USERAGENT, $agent);
-
-			$data = curl_exec($ch);
-			$res = json_decode($data);
-
-			return $res;
-		}
-		
 	?>
 	<div class="container">
 		<div class="row">
@@ -109,7 +30,7 @@
 		</div>
 		
 		<?php
-			$response = general();
+			$response = topHeadlines();
 			foreach($response->articles as $news) {
 		?>
 		<div class="row new bg-dark">
